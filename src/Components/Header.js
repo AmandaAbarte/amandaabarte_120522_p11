35892e0bf973fce36e7546336b3c6d66e9320174
AmandaAbarte <amanda.abarte@gmail.com>
2022-05-12 22:28:37 +0200

    header styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,22 @@
-
 import React from "react";
 
-import background from '../Assets/Background.png'
+import background from "../Assets/Background.png";
+import pinklogo from "../Assets/pinklogo.png";
 
-export default function Header(){
-    return(<header>
-        <nav className="nav">
-          <div className='logo'>KASA LOGO</div>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-          </ul>
-        </nav>
-        <img src={background}></img>
-        <p>Home Anywhere and Everywhere</p>
-      </header>)
-    
+export default function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img src={pinklogo} className="logo"></img>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </nav>
+      <div className="heading">
+          <img src={background}></img>
+          <p>Home Anywhere and Everywhere</p>
+      </div>
+    </header>
+  );
 }
