25746b9fc4a730925e0cc0ae1ee63ab3501890bd
AmandaAbarte <amanda.abarte@gmail.com>
2022-05-12 18:56:01 +0200

    basic skeleton

---
+++ src/Components/Accomodations.js
@@ -0,0 +1,14 @@
+import React from "react";
+export default function Accommodations() {
+  return (
+    <main>
+      <div className="accommodation-container">
+        <div className="accommodation-card"></div>
+        <div className="accommodation-card"></div>
+        <div className="accommodation-card"></div>
+        <div className="accommodation-card"></div>
+        <div className="accommodation-card"></div>
+      </div>
+    </main>
+  );
+}
---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+import background from '../Assets/Background.png'
+
+export default function Header(){
+    return(<header>
+        <nav class="nav">
+          <div className='logo'>KASA LOGO</div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+          </ul>
+        </nav>
+        <img src={background}></img>
+        <p>Home Anywhere and Everywhere</p>
+      </header>)
+    
+}
