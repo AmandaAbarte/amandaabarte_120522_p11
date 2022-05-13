7e65afb9530d4d9aae3fb5e5f00ed3499ff03baf
AmandaAbarte <amanda.abarte@gmail.com>
2022-05-13 15:20:25 +0200

    styled error page

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Error(){
     return(
-        <h1>404 ERROR</h1>
+        <div className="error">
+            <h1>404</h1>
+            <p>Oops! It looks like this page doesn't exist</p>
+            <Link to="/"> Go Back to home page</Link>
+        </div>
     )
 }
